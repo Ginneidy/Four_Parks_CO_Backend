270c8edce460d657b3e043d96cde30fc09304b6b
londono27 <0327miguel@gmail.com>
2024-05-09 12:11:50 -0500

    coors

--- Four_Parks_CO/settings/development.py
+++ Four_Parks_CO/settings/development.py
@@ -10,3 +10,28 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000",
+
+]
+
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
