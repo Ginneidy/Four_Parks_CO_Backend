26626d80b7225b6fad136969dce153686f987188
Ginneidy <ginneidy@gmail.com>
2024-05-26 20:50:08 -0500

    aaaa

--- Four_Parks_CO/settings/production.py
+++ Four_Parks_CO/settings/production.py
@@ -6,9 +6,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "fourParks",
-        "USER": "postgres",
-        "PASSWORD": "root",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "USER": "avnadmin",
+        "PASSWORD": "AVNS_lbb15Vs7IkJnNCFD6c7",
+        "HOST": "pg-48e5b0c4-e8a5-456f-b213-5a6b94abe153-fourpark3518781457-chor.i.aivencloud.com",
+        "PORT": "26810",
     }
 }
