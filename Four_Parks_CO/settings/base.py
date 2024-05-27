cdfbbca6d2482bac906f1069b2be788ad4a1e4d4
Ginneidy <ginneidy@gmail.com>
2024-05-27 14:59:54 -0500

    fix errors

--- Four_Parks_CO/settings/base.py
+++ Four_Parks_CO/settings/base.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # False for production
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
