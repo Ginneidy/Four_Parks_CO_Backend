eff4145271e8e78f6bfb1bd2db631f5b916a5306
londono27 <0327miguel@gmail.com>
2024-05-30 20:32:11 -0500

    arreglatesesamo

--- Four_Parks_CO/settings.py
+++ Four_Parks_CO/settings.py
@@ -68,18 +68,18 @@ TEMPLATES = [
 WSGI_APPLICATION = "Four_Parks_CO.wsgi.application"
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.environ.get("DB_NAME"),
+#         "USER": os.environ.get("DB_USER"),
+#         "PASSWORD": os.environ.get("DB_PASSWORD"),
+#         "HOST": os.environ.get("DB_HOST"),
+#         "PORT": os.environ.get("DB_PORT"),
+#     }
+# }
 
-""" DATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "fourParks",
@@ -88,7 +88,7 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "5432",
     }
-} """
+} 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
